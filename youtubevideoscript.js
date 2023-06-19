ab2d523049047e4c094c39020fd99453d9d9cc82
sampom <69758898+sampom@users.noreply.github.com>
2023-06-19 13:12:27 -0400

    Update youtubevideoscript.js

--- youtubevideoscript.js
+++ youtubevideoscript.js
@@ -8,7 +8,7 @@
     }
 
     function loadClient() {
-      gapi.client.setApiKey('YOUR_API_KEY');
+      gapi.client.setApiKey('AIzaSyCDdWFSnchcMNyA6dmP7fOaGCa8nt7dHIA');
       return gapi.client.load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest')
         .then(function() { console.log('GAPI client loaded for API'); },
               function(err) { console.error('Error loading GAPI client for API', err); });
@@ -17,7 +17,7 @@
     function execute() {
       return gapi.client.youtube.activities.list({
         "part": "snippet,contentDetails",
-        "channelId": "YOUR_CHANNEL_ID",
+        "channelId": "UCELOEM89sGzB2YRqbN8-uXg",
         "maxResults": 5
       })
         .then(function(response) {
@@ -33,7 +33,7 @@
     }
 
     gapi.load('client:auth2', function() {
-      gapi.auth2.init({ client_id: 'YOUR_CLIENT_ID' });
+      gapi.auth2.init({ client_id: '232304477126-a7731ui1fl2n7au0bbken1kidu21gpd2.apps.googleusercontent.com' });
     });
   </script>
 </head>
